2c26ed21de013d3598e35ec44ed2c480de82958c
Sangeeta-kurmi <sangitakurmi320@gmail.com>
2025-07-25 21:57:22 +0530

    Add files via upload

---
+++ if else task 1.py
@@ -0,0 +1,8 @@
+# Task 1: Check if a Number is Even or Odd
+
+a=int(input('inter a number: '))
+
+if (a // 2) * 2 == a:
+    print(f"{a} is an even number")
+else:
+    print(f"{a} is an odd number")
